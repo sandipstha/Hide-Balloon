ec815221b73600155bf370e2df4b2ae1eae1d138
Sandip Shrestha <sandipshrestha@Sandips-MacBook-Pro.local>
2016-10-22 11:31:45 -0700

    Added files for HideBalloon

--- HideBalloon/ViewController.swift
+++ HideBalloon/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var blueBalloon: UIImageView!
+    
+    @IBOutlet var redBalloon: UIImageView!
+    
+    @IBOutlet var redBalloonButton: UIButton!
+    
+    
+    @IBOutlet var blueBalloonButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +28,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    @IBAction func hideRedBalloon(sender: AnyObject) {
+        redBalloon.hidden = true
+        blueBalloon.hidden = false
+        redBalloonButton.hidden = true
+        blueBalloonButton.hidden = false
+    }
 
+    @IBAction func hideBlueBalloon(sender: AnyObject) {
+        redBalloon.hidden = false
+        blueBalloon.hidden = true
+        blueBalloonButton.hidden = true
+        redBalloonButton.hidden = false
+    }
 }
 
